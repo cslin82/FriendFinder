306ae911aa66915818bd5f49c653bccb71fe74be
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-05-11 17:24:30 -0400

    Populate themed friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,50 @@
-module.exports = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [
-        5,
-        1,
-        4,
-        4,
-        5,
-        1,
-        2,
-        5,
-        4,
-        1
-    ]
-}
-];
+module.exports = [ {
+    "name": "Robin",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 1, 4, 1, 3, 2, 3, 1, 3, 2, 4 ]
+},
+    {
+    "name": "Victoria",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 2, 2, 3, 2, 3, 5, 1, 5, 4, 1 ]
+},
+    {
+    "name": "Zoey",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 2, 5, 4, 5, 2, 3, 2, 1, 2, 4 ]
+},
+    {
+    "name": "Cindy",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 4, 2, 1, 3, 2, 3, 3, 1, 2, 5 ]
+},
+    {
+    "name": "Trudy",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 3, 3, 5, 3, 1, 4, 2, 4, 2, 5 ]
+},
+    {
+    "name": "Karen",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 3, 1, 4, 1, 4, 1, 3, 3, 1, 3 ]
+},
+    {
+    "name": "Stella",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 3, 2, 5, 3, 3, 3, 5, 3, 2, 4 ]
+},
+    {
+    "name": "Becky",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 4, 3, 1, 4, 3, 3, 4, 5, 4, 5 ]
+},
+    {
+    "name": "Naomi",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 5, 1, 5, 1, 4, 2, 3, 1, 5, 3 ]
+},
+    {
+    "name": "Natalie",
+    "photo": "http://placehold.it/400x400",
+    "scores": [ 4, 3, 4, 3, 2, 2, 2, 2, 3, 2 ]
+} ];
