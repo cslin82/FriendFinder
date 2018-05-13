f4e003ab59ebc9786645718eca4dca45633f147a
Chris Lin <35543509+cslin82@users.noreply.github.com>
2018-05-12 20:56:42 -0400

    Add images, static route for iamges, populate friendList.photo

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,50 +1,50 @@
 module.exports = [ {
     "name": "Robin",
-    "photo": "http://placehold.it/400x400",
+    "photo": "/images/robin.jpg",
     "scores": [ 1, 4, 1, 3, 2, 3, 1, 3, 2, 4 ]
 },
     {
     "name": "Victoria",
-    "photo": "http://placehold.it/400x400",
+    "photo": "/images/victoria.jpg",
     "scores": [ 2, 2, 3, 2, 3, 5, 1, 5, 4, 1 ]
 },
     {
     "name": "Zoey",
-    "photo": "http://placehold.it/400x400",
+    "photo": "/images/zoey.jpg",
     "scores": [ 2, 5, 4, 5, 2, 3, 2, 1, 2, 4 ]
 },
     {
     "name": "Cindy",
-    "photo": "http://placehold.it/400x400",
+    "photo": "/images/cindy.jpg",
     "scores": [ 4, 2, 1, 3, 2, 3, 3, 1, 2, 5 ]
 },
     {
-    "name": "Trudy",
-    "photo": "http://placehold.it/400x400",
+    "name": "Jeanette",
+    "photo": "/images/jeanette.jpg",
     "scores": [ 3, 3, 5, 3, 1, 4, 2, 4, 2, 5 ]
 },
     {
-    "name": "Karen",
-    "photo": "http://placehold.it/400x400",
+    "name": "Cassie",
+    "photo": "/images/cassie.jpg",
     "scores": [ 3, 1, 4, 1, 4, 1, 3, 3, 1, 3 ]
 },
     {
     "name": "Stella",
-    "photo": "http://placehold.it/400x400",
+    "photo": "/images/stella.jpg",
     "scores": [ 3, 2, 5, 3, 3, 3, 5, 3, 2, 4 ]
 },
     {
     "name": "Becky",
-    "photo": "http://placehold.it/400x400",
+    "photo": "/images/becky.jpg",
     "scores": [ 4, 3, 1, 4, 3, 3, 4, 5, 4, 5 ]
 },
     {
     "name": "Naomi",
-    "photo": "http://placehold.it/400x400",
+    "photo": "/images/naomi.jpg",
     "scores": [ 5, 1, 5, 1, 4, 2, 3, 1, 5, 3 ]
 },
     {
-    "name": "Natalie",
-    "photo": "http://placehold.it/400x400",
+    "name": "Carly",
+    "photo": "/images/carly.jpg",
     "scores": [ 4, 3, 4, 3, 2, 2, 2, 2, 3, 2 ]
 } ];
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -11,6 +11,11 @@ module.exports = function (app) {
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
+    
+    // should replace this with a static handler
+    app.get("/images/:name", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/images/" + req.params.name));
+    });
 
     // A default, catch-all route that leads to home.html which displays the home page. 
     app.get('*', function (req, res) {
